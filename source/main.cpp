8f5acba79f9548e336c1dcbdecb5cc502ea0dc6d
chintu <chintu_89@hotmail.com>
2023-03-03 13:31:00 +0100

    matrix multithreading implemented

--- source/main.cpp
+++ source/main.cpp
@@ -12,29 +12,45 @@ using namespace std;
 //double b[MATRIX_SIZE][MATRIX_SIZE];
 //double c[MATRIX_SIZE][MATRIX_SIZE];
 
-
+void create_rand_sqaurematrix(int m_size, vector<vector<double>>& vmatrix);
+void multiply_sqaureMatrices_T(vector<vector<double>>& inA,vector<vector<double>>& inB, vector<vector<double>>& resC);
+void multiply_sqaureMatrices_B(vector<vector<double>>& inA,vector<vector<double>>& inB, vector<vector<double>>& resC);
 
 int main(void)
 {
     vector<array<double,MATRIX_SIZE>> matA(MATRIX_SIZE);
     vector<array<double,MATRIX_SIZE>> matB(MATRIX_SIZE);
     vector<array<double,MATRIX_SIZE>> matC(MATRIX_SIZE);
+
+    vector<vector<double>> A(MATRIX_SIZE,vector<double>(MATRIX_SIZE,0));
+    vector<vector<double>> B(MATRIX_SIZE,vector<double>(MATRIX_SIZE,0));
+    vector<vector<double>> C(MATRIX_SIZE,vector<double>(MATRIX_SIZE,0));
+    create_rand_sqaurematrix(MATRIX_SIZE,A);
+    create_rand_sqaurematrix(MATRIX_SIZE,B);
     
     
     matrix_multiplication mm;
     mm.create_rand_sqaurematrix(matA,true);
     mm.create_rand_sqaurematrix(matB,true);
     mm.create_rand_sqaurematrix(matC,false);
-/*
-    mm.mul_sqaureMatrices(matA,matB,matC);
-    cout << "The execution time withouth optimization is: " << mm.get_execution_time() << "milliseconds" << endl;
+
+    //mm.mul_sqaureMatrices(matA,matB,matC);
+    //cout << "The execution time withouth optimization is: " << mm.get_execution_time() << "milliseconds" << endl;
     mm.mul_sqaureMatrices_byTranspose(matA,matB,matC);
     cout << "The execution time with transpose optimization is: " << mm.get_execution_time() << "milliseconds" << endl;
-*/  
+    mm.mul_sqaureMatrices_Blockwise(matA,matB,matC);
+    cout << "The execution time with block optimization is: " << mm.get_execution_time() << "milliseconds" << endl;
+    mm.mul_sqaureMatrices_multithread(4,matA,matB,matC);
+    cout << "The execution time with multithread optimization is: " << mm.get_execution_time() << "milliseconds" << endl;
+
+
+    multiply_sqaureMatrices_T(A,B,C);
+    multiply_sqaureMatrices_B(A,B,C);
+    /*
     Binomial_Coefficient bn(7,3);
     cout << "Result:" << bn.calculate_binCoeff() <<endl;
     cout << "Result:" << bn.calculate_binCoeff_opt1() <<endl;
-    /*
+    
     mm.mul_sqaureMatrices_byTranspose(MATRIX_SIZE,a,b,c);
     cout << "The execution time with transpose is: " << mm.get_execution_time() << "milliseconds";
 
@@ -48,8 +64,8 @@ int main(void)
     mm.delete_sqaurematrix(MATRIX_SIZE,c);
     */
 }
-/*
-void create_rand_sqaurematrix(int m_size, double ppmatrix[][MATRIX_SIZE])
+
+void create_rand_sqaurematrix(int m_size, vector<vector<double>>& vmatrix)
 {
     std::random_device rd;
     std::mt19937_64 e64(rd());
@@ -57,14 +73,22 @@ void create_rand_sqaurematrix(int m_size, double ppmatrix[][MATRIX_SIZE])
 
     for (int r = 0; r < m_size; r++) {
         for (int c = 0; c < m_size; c++){
-            ppmatrix[r][c]=dist(e64);
+            vmatrix[r][c]=dist(e64);
         }
     }
 }
 
-void multiply_sqaureMatrices(vector<array<double>>& inA,vector<array<double>>& inB, vector<array<double>>& resC)
+void multiply_sqaureMatrices_T(vector<vector<double>>& inA,vector<vector<double>>& inB, vector<vector<double>>& resC)
 {
     auto start = chrono::high_resolution_clock::now();
+    vector<vector<double>> b_transpose(MATRIX_SIZE,vector<double>(MATRIX_SIZE,0));
+
+    for(int i=0; i < MATRIX_SIZE; i++){
+        for(int j=0; j<MATRIX_SIZE; j++){
+            b_transpose[i][j]=inB[j][i];
+        }
+    }
+
     for(int i=0; i<MATRIX_SIZE; i++)
     {
         for(int j=0; j<MATRIX_SIZE; j++)
@@ -72,7 +96,7 @@ void multiply_sqaureMatrices(vector<array<double>>& inA,vector<array<double>>& i
             resC[i][j]=0;
             for( int k=0; k< MATRIX_SIZE; k++)
             {
-             resC[i][j] += inA[i][k]*inB[k][j];
+             resC[i][j] += inA[i][k]*b_transpose[j][k];
             }
         }
     }
@@ -81,31 +105,34 @@ void multiply_sqaureMatrices(vector<array<double>>& inA,vector<array<double>>& i
      cout << "Time taken by function: "<< duration.count() << " milliseconds" << endl;
 }
 
-void multiply_sqaureMatrices_transpose(double inA[][MATRIX_SIZE],double inB[][MATRIX_SIZE], double resC[][MATRIX_SIZE])
+void multiply_sqaureMatrices_B(vector<vector<double>>& inA,vector<vector<double>>& inB, vector<vector<double>>& resC)
 {
     auto start = chrono::high_resolution_clock::now();
-
-    for(int i=0; i<MATRIX_SIZE; i++)
-    {
-        for(int j=0; j<MATRIX_SIZE; j++)
-        {
-            bT[i][j]=inB[j][i];
+    vector<vector<double>> b_transpose(MATRIX_SIZE,vector<double>(MATRIX_SIZE,0));
+    for(int i=0; i < MATRIX_SIZE; i++){
+        for(int j=0; j<MATRIX_SIZE; j++){
+            b_transpose[i][j]=inB[j][i];
         }
     }
-
-    for(int i=0; i<MATRIX_SIZE; i++)
+    for(int i=0; i < resC.size(); i=i+BLOCK_SIZE)
     {
-        for(int j=0; j<MATRIX_SIZE; j++)
+        for(int j=0; j<resC.size(); j=j+BLOCK_SIZE)
         {
-            resC[i][j]=0;
-            for( int k=0; k< MATRIX_SIZE; k++)
+            for(int k=i; k <i+BLOCK_SIZE; k++)
             {
-             resC[i][j] += inA[i][k]*bT[j][k];
+                for(int l=j; l < j+BLOCK_SIZE; l++)
+                {
+                    resC[k][l]=0;
+                    for( int x=0; x< resC.size(); x++)
+                    {
+                        resC[k][l] += inA[k][x]*b_transpose[l][x];
+                    }
+                }
+        
             }
         }
     }
      auto stop = chrono::high_resolution_clock::now();
      auto duration = chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-     cout << "Time taken by function 2: "<< duration.count() << " milliseconds" << endl;
+     cout << "Time taken by function: "<< duration.count() << " milliseconds" << endl;
 }
-*/
--- source/matrix_multiplication.h
+++ source/matrix_multiplication.h
@@ -3,12 +3,15 @@
 #include<vector>
 #include<chrono>
 #include<array>
+#include<future>
 
 using namespace std;
 
 using namespace std::chrono_literals;
 
 #define MATRIX_SIZE 1024
+#define BLOCK_SIZE 32
+
 #define M(pointer,size,i,j) *((double*)pointer + (i*size) + j) 
 
 class matrix_multiplication{
@@ -20,8 +23,11 @@ class matrix_multiplication{
     template<size_t SIZE> void create_rand_sqaurematrix(vector<array<double,SIZE>>& matrix,bool fill);
     template<size_t SIZE> void mul_sqaureMatrices(vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC);
     template<size_t SIZE> void mul_sqaureMatrices_byTranspose(vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC);
+    template<size_t SIZE> void mul_sqaureMatrices_Blockwise(vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC);
+    template<size_t SIZE> void mul_sqaureMatrices_multithread(size_t threadCount,vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC);
     int get_execution_time();
-
+    template<size_t SIZE>void util_transposeMatrix(vector<array<double,SIZE>>& B,vector<array<double,SIZE>>& B_Transpose);
+    template<size_t SIZE> void util_dotProductRows(int firstIndex, int lastIndex, vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC);
 };
 
 template<size_t SIZE> void matrix_multiplication::create_rand_sqaurematrix(vector<array<double,SIZE>>& matrix,bool fill)
@@ -60,25 +66,92 @@ template<size_t SIZE> void matrix_multiplication::mul_sqaureMatrices_byTranspose
 {
     auto start = chrono::high_resolution_clock::now();
     vector<array<double,SIZE>> b_transpose(SIZE);
-    for(int i=0; i<SIZE; i++)
-    {
-        for(int j=0; j<SIZE; j++)
-        {
-            b_transpose[i][j]=inB[j][i];
-        }
-    }
+    this->util_transposeMatrix(inB,b_transpose);
+    this->util_dotProductRows(0,resC.size(),inA,b_transpose,resC);
+    
+    auto stop = chrono::high_resolution_clock::now();
+    this->duration = chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+}
 
-    for(int i=0; i<resC.size(); i++)
+template<size_t SIZE> void matrix_multiplication::mul_sqaureMatrices_Blockwise(vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC)
+{
+    auto start = chrono::high_resolution_clock::now();
+    vector<array<double,SIZE>> b_transpose(SIZE);
+    this->util_transposeMatrix(inB,b_transpose);
+    for(int i=0; i < resC.size(); i=i+BLOCK_SIZE)
     {
-        for(int j=0; j<resC.size(); j++)
+        for(int j=0; j<resC.size(); j=j+BLOCK_SIZE)
         {
-            resC[i][j]=0;
-            for( int k=0; k< resC.size(); k++)
-            {
-             resC[i][j] += inA[i][k]*b_transpose[j][k];
-            }
+            this->util_dotProductRows(i, i+BLOCK_SIZE,inA,b_transpose,resC);
         }
     }
     auto stop = chrono::high_resolution_clock::now();
     this->duration = chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+}
+
+template<size_t SIZE> void matrix_multiplication::mul_sqaureMatrices_multithread(size_t threadCount,vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC)
+{
+    auto start = chrono::high_resolution_clock::now();
+    vector<array<double,SIZE>> b_transpose(SIZE);
+    this->util_transposeMatrix(inB,b_transpose);
+
+    auto f_thread_dotproduct=[&inA, &b_transpose, &resC](size_t start, size_t last) {
+            for (size_t row = start; row < last; row++){
+                for (size_t col = 0; col < SIZE; col++){
+                    resC[row][col]=0;
+                    for (size_t col_t = 0; col_t < SIZE; col_t++){
+                        resC[row][col] += inA[row][col_t] * b_transpose[col][col_t];
+                    }
+                }
+            }
+        };
+
+    vector<size_t> row_seq;
+    size_t groupSize=SIZE / threadCount;
+    for(size_t thread_n=0; thread_n < SIZE; thread_n += groupSize){
+        row_seq.push_back(thread_n);
+    }
+
+    vector<std::future<void>> results; 
+    results.reserve(groupSize + 1);
+    for (size_t i = 0; i < row_seq.size() - 1; i++){
+        results.emplace_back(std::async(launch::async, f_thread_dotproduct, row_seq[i], row_seq[i + 1]));
+    }
+    for (auto& x : results){
+        x.get();
+    }
+
+    auto stop = chrono::high_resolution_clock::now();
+    this->duration = chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+}
+
+
+
+
+/**************************utility helper functions******************************************/
+template<size_t SIZE>void matrix_multiplication::util_transposeMatrix(vector<array<double,SIZE>>& B,vector<array<double,SIZE>>& B_Transpose)
+{
+    for(int i=0; i<SIZE; i++)
+    {
+        for(int j=0; j<SIZE; j++)
+        {
+            B_Transpose[i][j]=B[j][i];
+        }
+    }
+}
+
+template<size_t SIZE> void matrix_multiplication::util_dotProductRows(int firstIndex, int lastIndex, vector<array<double,SIZE>>& inA,vector<array<double,SIZE>>& inB, vector<array<double,SIZE>>& resC)
+{
+    for(int k=firstIndex; k <lastIndex; k++)
+            {
+                for(int l=firstIndex; l < lastIndex; l++)
+                {
+                    resC[k][l]=0;
+                    for( int x=0; x< resC.size(); x++)
+                    {
+                        resC[k][l] += inA[k][x]*inB[l][x];
+                    }
+                }
+        
+            }
 }
